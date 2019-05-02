d2abcdd8ffc0ba023ee04d170d2f9905ee6f66f9
abdullahalkharyeef <40389935+abdullahalkharyeef@users.noreply.github.com>
2019-05-02 12:58:34 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,300 @@
+
+    const hOneTitle = document.getElementById("hOneTitle");
+    hOneTitle.addEventListener('click', function GoHome (){
+      document.getElementById("ul1").style.display= 'block';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ulli1Q1").style.display= 'none';
+      document.getElementById("ulli1Q2").style.display= 'none';
+      document.getElementById("ulli1Q3").style.display= 'none';
+      document.getElementById("ulli1Q4").style.display= 'none';
+      document.getElementById("{Ved1}").style.display= 'none';
+    });
+
+    const liOne = document.getElementById("start");
+    liOne.addEventListener('click', function GoliOne (){
+      document.getElementById("ul1").style.display= 'none';
+      document.getElementById("ulli1").style.display= 'block';
+      document.getElementById("ulli1Q1").style.display= 'block';
+      document.getElementById("hFourReturn").style.display= 'block';
+    });
+
+
+    const hFourReturn = document.getElementById("hFourReturn");
+    hFourReturn.addEventListener('click', function ReturnHome (){
+      document.getElementById("ul1").style.display= 'block';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ulli1Q1").style.display= 'none';
+      document.getElementById("ulli1Q2").style.display= 'none';
+      document.getElementById("ulli1Q3").style.display= 'none';
+      document.getElementById("ulli1Q4").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+
+
+
+
+
+
+    // 1
+    const ulliOneQOneF1 = document.getElementById("ulli1Q1F1");
+    ulliOneQOneF1.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ul1").style.display= 'block';
+      document.getElementById("ulli1Q1").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+    });
+    const ulliOneQOneT = document.getElementById("ulli1Q1T");
+    ulliOneQOneT.addEventListener('click', function GoalertTrue (){
+      document.getElementById("ul1").style.display= 'none';
+      document.getElementById("ulli1Q1").style.display= 'none';
+      document.getElementById("ulli1Q2").style.display= 'block';
+    });
+
+
+
+    // 2
+    const ulliOneQTwoF1 = document.getElementById("ulli1Q2F1");
+    ulliOneQTwoF1.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q2").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQTwoF2 = document.getElementById("ulli1Q2F2");
+    ulliOneQTwoF2.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q2").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQTwoT = document.getElementById("ulli1Q2T");
+    ulliOneQTwoT.addEventListener('click', function GoalertTrue (){
+      document.getElementById("ulli1Q2").style.display= 'none';
+      document.getElementById("ulli1Q3").style.display= 'block';
+    });
+
+
+
+    // 3
+    const ulliOneQThreeF1 = document.getElementById("ulli1Q3F1");
+    ulliOneQThreeF1.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q3").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQThreeF2 = document.getElementById("ulli1Q3F2");
+    ulliOneQThreeF2.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q3").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQThreeT = document.getElementById("ulli1Q3T");
+    ulliOneQThreeT.addEventListener('click', function GoalertTrue (){
+      document.getElementById("ulli1Q3").style.display= 'none';
+      document.getElementById("ulli1Q4").style.display= 'block';
+    });
+
+
+
+    // 4
+    const ulliOneQFourF1 = document.getElementById("ulli1Q4F1");
+    ulliOneQFourF1.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q4").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQFourF2 = document.getElementById("ulli1Q4F2");
+    ulliOneQFourF2.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q4").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQFourT = document.getElementById("ulli1Q4T");
+    ulliOneQFourT.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q4").style.display= 'none';
+      document.getElementById("ulli1Q5").style.display= 'block';
+    });
+
+
+
+    // 5
+   const ulliOneQFiveF1 = document.getElementById("ulli1Q5F1");
+    ulliOneQFiveF1.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q5").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQFiveT = document.getElementById("ulli1Q5T");
+    ulliOneQFiveT.addEventListener('click', function GoalertTrue (){
+      document.getElementById("ulli1Q5").style.display= 'none';
+      document.getElementById("ulli1Q6").style.display= 'block';
+    });
+
+
+
+    // 6
+     const ulliOneQSixF1 = document.getElementById("ulli1Q6F1");
+    ulliOneQSixF1.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q6").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQSixF2 = document.getElementById("ulli1Q6F2");
+    ulliOneQSixF2.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q6").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQSixT = document.getElementById("ulli1Q6T");
+    ulliOneQSixT.addEventListener('click', function GoalertTrue (){
+      document.getElementById("ulli1Q6").style.display= 'none';
+      document.getElementById("ulli1Q7").style.display= 'block';
+    });
+
+
+
+    // 7
+    const ulliOneQSavenF1 = document.getElementById("ulli1Q7F1");
+    ulliOneQSavenF1.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q7").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQSavenF2 = document.getElementById("ulli1Q7F2");
+    ulliOneQSavenF2.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q7").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQSavenT = document.getElementById("ulli1Q7T");
+    ulliOneQSavenT.addEventListener('click', function GoalertTrue (){
+      document.getElementById("ulli1Q7").style.display= 'none';
+      document.getElementById("ulli1Q8").style.display= 'block';
+    });
+
+
+
+    // 8
+    const ulliOneQEightF1 = document.getElementById("ulli1Q8F1");
+    ulliOneQEightF1.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q8").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQEightF2 = document.getElementById("ulli1Q8F2");
+    ulliOneQEightF2.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q8").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQEightT = document.getElementById("ulli1Q8T");
+    ulliOneQEightT.addEventListener('click', function GoalertTrue (){
+      document.getElementById("ulli1Q8").style.display= 'none';
+      document.getElementById("ulli1Q9").style.display= 'block';
+    });
+
+
+
+// 9
+    const ulliOneQNineF1 = document.getElementById("ulli1Q9F1");
+    ulliOneQNineF1.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q9").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQNineF2 = document.getElementById("ulli1Q9F2");
+    ulliOneQNineF2.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q9").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQNineT = document.getElementById("ulli1Q9T");
+    ulliOneQNineT.addEventListener('click', function GoalertTrue (){
+      document.getElementById("ulli1Q9").style.display= 'none';
+      document.getElementById("ulli1Q10").style.display= 'block';
+    });
+
+
+
+// 10
+    const ulliOneQTenF1 = document.getElementById("ulli1Q10F1");
+    ulliOneQTenF1.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q10").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQTenF2 = document.getElementById("ulli1Q10F2");
+    ulliOneQTenF2.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q10").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQTenT = document.getElementById("ulli1Q10T");
+    ulliOneQTenT.addEventListener('click', function GoalertTrue (){
+      document.getElementById("ulli1Q10").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ulli1Q11").style.display= 'block';
+    });
+
+
+
+// 11
+    const ulliOneQElevenF1 = document.getElementById("ulli1Q11F1");
+    ulliOneQElevenF1.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q11").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQElevenF2 = document.getElementById("ulli1Q11F2");
+    ulliOneQElevenF2.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q11").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQElevenT = document.getElementById("ulli1Q11T");
+    ulliOneQElevenT.addEventListener('click', function GoalertTrue (){
+      document.getElementById("ulli1Q11").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ulli1Q12").style.display= 'block';
+    });
+
+
+
+// 12
+    const ulliOneQTwelveF1 = document.getElementById("ulli1Q12F1");
+    ulliOneQTwelveF1.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q12").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQTwelveF2 = document.getElementById("ulli1Q12F2");
+    ulliOneQElevenF2.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q12").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQTwelveT = document.getElementById("ulli1Q12T");
+    ulliOneQTwelveT.addEventListener('click', function GoalertTrue (){
+      document.getElementById("ulli1Q12").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ulli1Q13").style.display= 'block';
+    });
+
+
+
+// 12
+    const ulliOneQThirteenF1 = document.getElementById("ulli1Q13F1");
+    ulliOneQThirteenF1.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q13").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQThirteenF2 = document.getElementById("ulli1Q13F2");
+    ulliOneQThirteenF2.addEventListener('click', function GoalertFlase (){
+      document.getElementById("ulli1Q13").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("ul1").style.display= 'block';
+    });
+    const ulliOneQThirteenT = document.getElementById("ulli1Q13T");
+    ulliOneQTwelveT.addEventListener('click', function GoalertTrue (){
+      document.getElementById("ulli1Q13").style.display= 'none';
+      document.getElementById("hFourReturn").style.display= 'none';
+      document.getElementById("Ved1").style.display= 'block';
+    });
